a978cb727deb16f0aa8ba07eb6107b12fa314cd5
Tanush T M <tanushtm.work@gmail.com>
2025-07-16 20:40:53 +0530

    data added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import os
 from langchain_openai import OpenAIEmbeddings
 from dotenv import load_dotenv
 from sklearn.metrics.pairwise import cosine_similarity
-import numpy as np
 
 load_dotenv()
 
@@ -10,21 +10,35 @@ embedding = OpenAIEmbeddings(
     dimensions=300
 )
 
-documents = [
-    "",
-    
-]
+DOCS_FOLDER = "docs"
+documents = []
+filenames = []
 
-query = ""
+for filename in os.listdir(DOCS_FOLDER):
+    if filename.endswith(".txt"):
+        with open(os.path.join(DOCS_FOLDER, filename), "r", encoding="utf-8") as f:
+            documents.append(f.read())
+            filenames.append(filename)
+
+if not documents:
+    print("No documents found in the 'docs' folder.")
+    exit(1)
+
+query = input("Enter your query: ").strip()
+if not query:
+    print("Query cannot be empty.")
+    exit(1)
 
 docs_embedding = embedding.embed_documents(documents)
 query_embedding = embedding.embed_query(query)
 
 scores = cosine_similarity([query_embedding], docs_embedding)[0]
 
-index, score = sorted(list(enumerate(scores)),key=lambda x:x[1])[-1]
+index = scores.argmax()
+score = scores[index]
 
-print (query)
-print (documents [index])
-print("similarity score is:", score)
-
+print("\nQuery:")
+print(query)
+print("\nMost similar document: {}".format(filenames[index]))
+print(documents[index][:500] + ("..." if len(documents[index]) > 500 else ""))
+print("\nSimilarity score: {:.4f}".format(score))
